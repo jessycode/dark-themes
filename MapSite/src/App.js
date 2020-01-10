ee0bbf0e68a222f1ac4c42c5fbca6a2e27e28d76
GislaineJessica <je551c4hall0@gmail.com>
2020-01-10 14:42:07 -0300

    docs: Reesturando os arquivos

--- MapSite/src/App.js
+++ MapSite/src/App.js
@@ -4,7 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
- 
+      <div className="App-content">
+        <div className="Nav">
+         <a href="/"> Home </a>
+         <a href="/"> Home </a>
+         <a href="/"> Home </a>
+         <a href="/"> Home </a>
+       </div>
+        <div className="info">
+          <h1>
+            Fatos Interessantes sobre os mamiferos
+          </h1>
+          <h2>
+            Algum paragrafo esplicando algo interessante. 
+            Isso é interessante para saber como visualizar os espaçamentos entre os elementos
+          </h2>
+          <h4>
+            É de graça, chega mais!!
+          </h4>
+          <h5>
+            Venha se conectar.
+          </h5>
+        </div>
+      </div>
+
     </div>
   );
 }
